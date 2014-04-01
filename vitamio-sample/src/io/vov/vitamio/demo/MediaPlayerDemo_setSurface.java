1cceded0f356250f1956f65277d487b0e2b25678
Crossle Song <sxm@yixia.com>
2014-04-01 17:30:16 +0800

    TextureView add adjust AspectRatio

--- vitamio-sample/src/io/vov/vitamio/demo/MediaPlayerDemo_setSurface.java
+++ vitamio-sample/src/io/vov/vitamio/demo/MediaPlayerDemo_setSurface.java
@@ -18,6 +18,7 @@ package io.vov.vitamio.demo;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Matrix;
 import android.graphics.SurfaceTexture;
 import android.media.AudioManager;
 import android.os.Bundle;
@@ -36,130 +37,161 @@ import io.vov.vitamio.MediaPlayer.OnPreparedListener;
 public class MediaPlayerDemo_setSurface extends Activity implements OnBufferingUpdateListener,
     OnCompletionListener, OnPreparedListener, TextureView.SurfaceTextureListener {
 
-	private static final String TAG = "MediaPlayerDemo";
-	private int mVideoWidth;
-	private int mVideoHeight;
-	private MediaPlayer mMediaPlayer;
-	private TextureView mTextureView;
-	private String path;
+  private static final String TAG = "MediaPlayerDemo";
+  private int mVideoWidth;
+  private int mVideoHeight;
+  private MediaPlayer mMediaPlayer;
+  private TextureView mTextureView;
+  private String path;
 
-	private boolean mIsVideoSizeKnown = false;
-	private boolean mIsVideoReadyToBePlayed = false;
+  private boolean mIsVideoSizeKnown = false;
+  private boolean mIsVideoReadyToBePlayed = false;
 
-	/**
-	 * 
-	 * Called when the activity is first created.
-	 */
-	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		if (!LibsChecker.checkVitamioLibs(this))
-			return;
-		setContentView(R.layout.mediaplayer_3);
-		mTextureView = (TextureView) findViewById(R.id.surface);
-		mTextureView.setSurfaceTextureListener(this);
+  /**
+   * 
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    if (!LibsChecker.checkVitamioLibs(this))
+      return;
+    setContentView(R.layout.mediaplayer_3);
+    mTextureView = (TextureView) findViewById(R.id.surface);
+    mTextureView.setSurfaceTextureListener(this);
 
-	}
+  }
 
-	@SuppressLint("NewApi")
-	private void playVideo(SurfaceTexture surfaceTexture) {
-		doCleanUp();
-		try {
+  @SuppressLint("NewApi")
+  private void playVideo(SurfaceTexture surfaceTexture) {
+    doCleanUp();
+    try {
 
-			path = "";
-			if (path == "") {
-				// Tell the user to provide a media file URL.
-				Toast.makeText(
-				    MediaPlayerDemo_setSurface.this,
-				    "Please edit MediaPlayerDemo_setSurface Activity, "
-				        + "and set the path variable to your media file path."
-				        + " Your media file must be stored on sdcard.", Toast.LENGTH_LONG).show();
-				return;
-			}
-			// Create a new media player and set the listeners
-			mMediaPlayer = new MediaPlayer(this);
-			mMediaPlayer.setDataSource(path);
-			mMediaPlayer.setSurface(new Surface(surfaceTexture));
-			mMediaPlayer.prepareAsync();
-			mMediaPlayer.setOnBufferingUpdateListener(this);
-			mMediaPlayer.setOnCompletionListener(this);
-			mMediaPlayer.setOnPreparedListener(this);
-			setVolumeControlStream(AudioManager.STREAM_MUSIC);
+      path = "";
+      if (path == "") {
+        // Tell the user to provide a media file URL.
+        Toast.makeText(
+            MediaPlayerDemo_setSurface.this,
+            "Please edit MediaPlayerDemo_setSurface Activity, "
+                + "and set the path variable to your media file path."
+                + " Your media file must be stored on sdcard.", Toast.LENGTH_LONG).show();
+        return;
+      }
+      // Create a new media player and set the listeners
+      mMediaPlayer = new MediaPlayer(this, true);
+      mMediaPlayer.setDataSource(path);
+      mMediaPlayer.setSurface(new Surface(surfaceTexture));
+      mMediaPlayer.prepareAsync();
+      mMediaPlayer.setOnBufferingUpdateListener(this);
+      mMediaPlayer.setOnCompletionListener(this);
+      mMediaPlayer.setOnPreparedListener(this);
+      setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-		} catch (Exception e) {
-			Log.e(TAG, "error: " + e.getMessage(), e);
-		}
-	}
+    } catch (Exception e) {
+      Log.e(TAG, "error: " + e.getMessage(), e);
+    }
+  }
 
-	public void onBufferingUpdate(MediaPlayer arg0, int percent) {
-		// Log.d(TAG, "onBufferingUpdate percent:" + percent);
+  public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+    // Log.d(TAG, "onBufferingUpdate percent:" + percent);
 
-	}
+  }
 
-	public void onCompletion(MediaPlayer arg0) {
-		Log.d(TAG, "onCompletion called");
-	}
+  public void onCompletion(MediaPlayer arg0) {
+    Log.d(TAG, "onCompletion called");
+  }
 
+  public void onPrepared(MediaPlayer mediaplayer) {
+    Log.d(TAG, "onPrepared called");
+    mIsVideoReadyToBePlayed = true;
+    if (mIsVideoReadyToBePlayed) {
+      startVideoPlayback();
+    }
+  }
 
-	public void onPrepared(MediaPlayer mediaplayer) {
-		Log.d(TAG, "onPrepared called");
-		mIsVideoReadyToBePlayed = true;
-		if (mIsVideoReadyToBePlayed) {
-			startVideoPlayback();
-		}
-	}
+  @Override
+  protected void onPause() {
+    super.onPause();
+    releaseMediaPlayer();
+    doCleanUp();
+  }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		releaseMediaPlayer();
-		doCleanUp();
-	}
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    releaseMediaPlayer();
+    doCleanUp();
+  }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		releaseMediaPlayer();
-		doCleanUp();
-	}
+  private void releaseMediaPlayer() {
+    if (mMediaPlayer != null) {
+      mMediaPlayer.release();
+      mMediaPlayer = null;
+    }
+  }
 
-	private void releaseMediaPlayer() {
-		if (mMediaPlayer != null) {
-			mMediaPlayer.release();
-			mMediaPlayer = null;
-		}
-	}
+  private void doCleanUp() {
+    mVideoWidth = 0;
+    mVideoHeight = 0;
+    mIsVideoReadyToBePlayed = false;
+    mIsVideoSizeKnown = false;
+  }
 
-	private void doCleanUp() {
-		mVideoWidth = 0;
-		mVideoHeight = 0;
-		mIsVideoReadyToBePlayed = false;
-		mIsVideoSizeKnown = false;
-	}
+  private void startVideoPlayback() {
+    Log.v(TAG, "startVideoPlayback");
+    adjustAspectRatio(mMediaPlayer.getVideoWidth(), mMediaPlayer.getVideoHeight());
+    mMediaPlayer.start();
+  }
 
-	private void startVideoPlayback() {
-		Log.v(TAG, "startVideoPlayback");
-		mMediaPlayer.start();
-	}
+  /**
+   * Sets the TextureView transform to preserve the aspect ratio of the video.
+   */
+  private void adjustAspectRatio(int videoWidth, int videoHeight) {
+    int viewWidth = mTextureView.getWidth();
+    int viewHeight = mTextureView.getHeight();
+    double aspectRatio = (double) videoHeight / videoWidth;
 
-	@Override
+    int newWidth, newHeight;
+    if (viewHeight > (int) (viewWidth * aspectRatio)) {
+      // limited by narrow width; restrict height
+      newWidth = viewWidth;
+      newHeight = (int) (viewWidth * aspectRatio);
+    } else {
+      // limited by short height; restrict width
+      newWidth = (int) (viewHeight / aspectRatio);
+      newHeight = viewHeight;
+    }
+    int xoff = (viewWidth - newWidth) / 2;
+    int yoff = (viewHeight - newHeight) / 2;
+    Log.v(TAG, "video=" + videoWidth + "x" + videoHeight + " view=" + viewWidth + "x" + viewHeight
+        + " newView=" + newWidth + "x" + newHeight + " off=" + xoff + "," + yoff);
+
+    Matrix txform = new Matrix();
+    mTextureView.getTransform(txform);
+    txform.setScale((float) newWidth / viewWidth, (float) newHeight / viewHeight);
+    //txform.postRotate(10);          // just for fun
+    txform.postTranslate(xoff, yoff);
+    mTextureView.setTransform(txform);
+  }
+
+  @Override
   public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-		playVideo(surface);
+    playVideo(surface);
   }
 
-	@Override
+  @Override
   public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-	  
+
   }
 
-	@Override
+  @Override
   public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-	  return false;
+    return false;
   }
 
-	@Override
+  @Override
   public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-	  
+
   }
 
 }
